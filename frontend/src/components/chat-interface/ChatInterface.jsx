02edcee4b5158a560b7ad5efc041fb3c9194fbdf
Sandeep Varma <sandeepvarma531@gmail.com>
2025-02-20 16:32:31 -0600

    changes for css and responsive

--- frontend/src/components/chat-interface/ChatInterface.jsx
+++ frontend/src/components/chat-interface/ChatInterface.jsx
@@ -1,63 +1,53 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./chat.css";
 import SendIcon from '@mui/icons-material/Send';
-import MenuIcon from '@mui/icons-material/Menu';
 import TextInput from "../common/textinput/TextInput";
-import { InputAdornment } from "@mui/material";
 
-const ChatInterface = () => {
-  const [messages, setMessages] = useState([]);
+const ChatInterface = ({ ...props }) => {
+  // const [messages, setMessages] = useState([]);
   const [input, setInput] = useState("");
-  const [sidebarVisible, setSidebarVisible] = useState(true);
 
-  const lastMessageRef = useRef(null);
+  // const lastMessageRef = useRef(null);
 
-  useEffect(() => {
-    if (lastMessageRef.current) {
-      lastMessageRef.current.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "nearest" });
-    }
-  }, [messages]);
+  // useEffect(() => {
+  //   if (lastMessageRef.current) {
+  //     lastMessageRef.current.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "nearest" });
+  //   }
+  // }, [messages]);
 
   const sendMessage = () => {
-    if (input.trim()) {
-      setMessages((prevMessages) => [...prevMessages, { text: input, sender: "user" }]);
-      setTimeout(() => {
-        setMessages((prevMessages) => [...prevMessages, { text: "This is a bot response", sender: "bot" }]);
-      }, 1000);
+      props.sendMessage(input);
       setInput("");
-    }
   };
 
   return (
-    <>
-      <div className="chat-container">
-        <div className="chat">
-          <div className="chat-messages">
-            {messages.map((msg, index) => (
-              <div key={index} ref={index === messages.length - 1 ? lastMessageRef : null}
-                className={`message ${msg.sender === "user" ? "sent" : "received"}`}>
-                {msg.text}
-              </div>
-            ))}
-          </div>
-          
-          <div className="chat-input">
-            <TextInput
-              type="text" 
-              // placeholder="Type a message..." 
-              label="Message your Agent"
-              value={input} 
-              fullWidth
-              onChange={(e) => setInput(e.target.value)}
-              onKeyDown={(e) => e.key === "Enter" && sendMessage()}
-            />
-            <button className="send-button" onClick={sendMessage}>
-              <SendIcon size={20} />
-            </button>
-          </div>
+    <div className="chat-container">
+      <div className="chat">
+        <div className="chat-messages">
+          {props.messages.map((msg, index) => (
+            <div key={index} ref={index === props.messages.length - 1 ? props.lastMessageRef : null}
+              className={`message ${msg.sender === "user" ? "sent" : "received"}`}>
+              {msg.text}
+            </div>
+          ))}
+        </div>
+        
+        <div className="chat-input">
+          <TextInput
+            type="text" 
+            // placeholder="Type a message..." 
+            label="Message your Agent"
+            value={input} 
+            fullWidth
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+          />
+          <button className="send-button" onClick={sendMessage}>
+            <SendIcon size={20} />
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- frontend/src/components/vita-login-page/VitaLoginPage.jsx
+++ frontend/src/components/vita-login-page/VitaLoginPage.jsx
@@ -1,68 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import './vitaloginpage.css';
 import MenuIcon from '@mui/icons-material/Menu';
 import Header from "../common/header/Header";
 import Footer from "../common/footer/Footer";
 import Main from "./components/main/Main";
-import { Home, Sms, Bookmark, Settings, AccountTree, KeyboardArrowUp, KeyboardArrowDown, AddCircleOutline, Schedule } from '@mui/icons-material';
+import { AccountTree, KeyboardArrowUp, KeyboardArrowDown, AddCircleOutline, Schedule } from '@mui/icons-material';
 import Project from "./components/projects/Project";
 import ChatInterface from "../chat-interface/ChatInterface";
-
-const styles = {
-    nav: {
-      display: "flex",
-      flexDirection: "column",
-      gap: "15px",
-    },
-    navItem: {
-      color: "black",
-      textDecoration: "none",
-      display: "flex",
-      alignItems: "center",
-      gap: "10px",
-      fontSize: "16px",
-      padding: "5px",
-      borderRadius: "8px",
-      transition: "background 0.2s",
-    },
-    navItemHover: {
-      backgroundColor: "#2c2f33",
-    },
-    profileSection: {
-      marginTop: "auto",
-      display: "flex",
-      alignItems: "center",
-      gap: "10px",
-      padding: "10px",
-      borderTop: "1px solid #444",
-    },
-    profileIcon: {
-      width: "40px",
-      height: "40px",
-      backgroundColor: "#444",
-      borderRadius: "50%",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      fontSize: "18px",
-    },
-    profileName: {
-      fontSize: "16px",
-    },
-    projectList: {
-        display: "flex",
-        flexDirection: "column",
-        paddingLeft: "30px",
-        gap: "10px",
-    },
-    projectItem: {
-        color: "black",
-        textDecoration: "none",
-        fontSize: "14px",
-        padding: "0px 0",
-        textAlign: "left"
-    },
-};
 
 const STATES = {
     MAIN: 'main',
@@ -71,23 +15,39 @@ const STATES = {
 }
 
 const VitaLoginPage = () => {
-    const [messages, setMessages] = useState([]);
-    const [input, setInput] = useState("");
+    const [chatId, setChatId] = useState("");
     const [sidebarVisible, setSidebarVisible] = useState(true);
     const [isProjectsExpanded, setIsProjectsExpanded] = useState(false);
     const [state, setState] = useState(STATES.MAIN);
     const [newProject, setNewProject] = useState(false);
 
-    const sendMessage = () => {
-        if (input.trim()) {
-            setMessages((prevMessages) => [...prevMessages, { text: input, sender: "user" }]);
+    const [messages, setMessages] = useState([]);
+    const lastMessageRef = useRef(null);
+
+    useEffect(() => {
+        if (lastMessageRef.current) {
+          lastMessageRef.current.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "nearest" });
+        }
+    }, [messages]);
+
+    const getAllMessages = (chatId) => {
+        if (chatId.trim()) {
+            setMessages([{ text: chatId, sender: "user" }]);
             setTimeout(() => {
-            setMessages((prevMessages) => [...prevMessages, { text: "This is a bot response", sender: "bot" }]);
+              setMessages((prevMessages) => [...prevMessages, { text: "This is a bot response", sender: "bot" }]);
             }, 1000);
-            setInput("");
+          }
+    }
+
+    const sendMessage = (input) => {
+        if (input.trim()) {
+          setMessages((prevMessages) => [...prevMessages, { text: input, sender: "user" }]);
+          setTimeout(() => {
+            setMessages((prevMessages) => [...prevMessages, { text: "This is a bot response", sender: "bot" }]);
+          }, 1000);
         }
     };
-    
+
     return (
         <>
             <Header />
@@ -95,27 +55,41 @@ const VitaLoginPage = () => {
                 <div className={`sidebar ${sidebarVisible ? "visible" : ""}`}>
                     <button className="close-sidebar" onClick={() => setSidebarVisible(false)}>✖</button>
                     <div className="options-container">
-                        <nav style={styles.nav}>
-                            <div style={styles.navItem} onClick={() => {setNewProject(false);setState(STATES.PROJECT)}}>
+                        <nav className="sidebar-nav">
+                            <div className="sidebar-navItem" onClick={() => {setNewProject(false);setState(STATES.PROJECT)}}>
                                 <AccountTree /> 
                                 Projects 
                                 {isProjectsExpanded ? <KeyboardArrowUp onClick={() => setIsProjectsExpanded(!isProjectsExpanded)} /> : <KeyboardArrowDown onClick={() => setIsProjectsExpanded(!isProjectsExpanded)} />}
                                 <AddCircleOutline onClick={(event) => {event.stopPropagation();setNewProject(true);setState(STATES.PROJECT)}}/>
                             </div>
                             {isProjectsExpanded && (
-                            <div style={styles.projectList}>
-                                <a href="#" style={styles.projectItem}> Project-1</a>
-                                <a href="#" style={styles.projectItem}> Project-2</a>
-                                <a href="#" style={styles.projectItem}> Project-3</a>
-                            </div>
+                                <div className="sidebar-project-list">
+                                    <a href="#" className="sidebar-project-item"> Project-1</a>
+                                    <a href="#" className="sidebar-project-item"> Project-2</a>
+                                    <a href="#" className="sidebar-project-item"> Project-3</a>
+                                </div>
                             )}
                             {/* <a href="#" style={styles.navItem}><Bookmark /> Saved</a>
                             <a href="#" style={styles.navItem}><Settings /> Settings</a> */}
-                            <a href="#" onClick={() => setState(STATES.RECENT)} style={styles.navItem}><Schedule /> Recent Chats</a>
+                            <a className="sidebar-navItem"><Schedule /> Recent Chats</a>
+                            <div className="sidebar-project-list">
+                                <a className="sidebar-project-item"
+                                    onClick={() => {setState(STATES.RECENT);getAllMessages("Project-1");setChatId("Project-1");}}>
+                                    Project-1
+                                </a>
+                                <a className="sidebar-project-item"
+                                    onClick={() => {setState(STATES.RECENT);getAllMessages("Project-2");setChatId("Project-2");}}>
+                                    Project-2
+                                </a>
+                                <a className="sidebar-project-item"
+                                    onClick={() => {setState(STATES.RECENT);getAllMessages("Project-3");setChatId("Project-3");}}>
+                                    Project-3
+                                </a>
+                            </div>
                         </nav>
-                        <div style={styles.profileSection}>
-                            <div style={styles.profileIcon}>M</div>
-                            <div style={styles.profileName}>Username</div>
+                        <div className="sidebar-profile-section">
+                            <div className="sidebar-profile-icon">M</div>
+                            <div className="sidebar-profile-name">Username</div>
                         </div>
                     </div>
                 </div>
@@ -129,7 +103,8 @@ const VitaLoginPage = () => {
                     {STATES.MAIN == state && <Main />}
                     {STATES.PROJECT == state && <Project newProject={newProject} />}
                     {/* {STATES.RECENT == state && <></>} */}
-                    {STATES.RECENT == state && <ChatInterface />}
+                    {STATES.RECENT == state && 
+                    <ChatInterface messages={messages} lastMessageRef={lastMessageRef} chatId={chatId} sendMessage={sendMessage} />}
                 </div>
             </div>
             <Footer />
