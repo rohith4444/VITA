91847e15f44256863bb7bc04968d07d967ff07f2
rmandala <rohithreddy.m4@gmail.com>
2025-03-02 16:01:45 -0500

    import chaneg in config file

--- chat_api/config.py
+++ chat_api/config.py
@@ -1,6 +1,6 @@
 import os
 from typing import Optional
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 from core.logging.logger import setup_logger
 
